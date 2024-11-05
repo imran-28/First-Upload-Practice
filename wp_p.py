1e9293ee1ac55134014b17b3cb1f24913593c463
imran-28 <noormr42@gmail.com>
2024-11-05 22:20:43 +0600

    wp post function updated

--- wp_p.py
+++ wp_p.py
@@ -1,10 +1,11 @@
-import wp_func
+from wp_func import wp_post
+from wp_func import heading_two
 content = r'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.'
 
 con2 = r'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.'
 
 head = 'Where can I get some?'
 
-post = wp_func.wp_post(content)+wp_func.heading_two(head)+ wp_func.wp_post(con2)
+post = wp_post(content)+heading_two(head)+ wp_post(con2)
 
 print(post)
